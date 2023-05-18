180167cf69b22c545ca7e18af26ce49e3571b7e2
valeraUA <116517026+reznyalviv@users.noreply.github.com>
2023-05-18 11:14:46 +0300

    Update Main4.java

--- Main4.java
+++ Main4.java
@@ -1,1 +1,10 @@
-
+public class Main4 {
+    public static void main(String[] args) {
+        int num = 10;
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        System.out.println(factorial);
+    }
+}
