fe55727136a24ecacaf72c9eb3d54f8da1deaaf4
valeraUA <116517026+reznyalviv@users.noreply.github.com>
2023-05-18 11:12:53 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 500; i <= 650; i += 10) {
+            System.out.print(i + " ");
+        }
 
+        //
+        int j = 500;
+        while (j <= 650) {
+            System.out.print(j + " ");
+            j += 10;
+        }
+
+        // do-while
+        int k = 500;
+        do {
+            System.out.print(k + " ");
+            k += 10;
+        } while (k <= 650);
+    }
+}
