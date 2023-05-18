b9baf378062fa4d5d7415b001410f47e7bee9cd0
valeraUA <116517026+reznyalviv@users.noreply.github.com>
2023-05-18 11:13:42 +0300

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,1 +1,10 @@
+public class Main2  {
+    public static void main(String[] args) {
 
+        int a = 2;
+        while (a < 5000) {
+            System.out.print(a + " ");
+            a = 2 * a - 1;
+        }
+    }
+}
