79bb5d4b9ab5ae99cbd60a3fc32c640bfc0ef862
valeraUA <116517026+reznyalviv@users.noreply.github.com>
2023-05-18 11:15:28 +0300

    Update Main5.java

--- Main5.java
+++ Main5.java
@@ -1,1 +1,13 @@
-
+public class Main5 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < 60; j++) {
+                if (i / 10 == j % 10 && i % 10 == j / 10) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
